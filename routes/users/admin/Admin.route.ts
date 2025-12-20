c478dded96c3797a084bd170c52db93eebaa9306
AbdoulrahmanAlAli <alaliabdoulrahman712@gmail.com>
2025-12-20 19:56:14 +0300

    Initial commit

--- routes/users/admin/Admin.route.ts
+++ routes/users/admin/Admin.route.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
 import { adminController } from "../../../controllers/users/admin/Admin.controller";
+import verifyToken from "../../../middlewares/verifyToken";
+import checkRole from "../../../middlewares/checkRole";
 
 const router: Router = Router();
 
 // Protected admin routes (require authentication)
-router.route("/create").post(adminController.createNewAdmin);
-router.route("/profile/:id").get(adminController.getProfile);
-router.route("/").get(adminController.getAllAdmins);
-router.route("/:id").get(adminController.getAdminById);
-router.route("/:id").put(adminController.updateAdmin);
-router.route("/:id").delete(adminController.deleteAdmin);
-router.route("/:id/change-password").put(adminController.changePassword);
+router
+  .route("/create")
+  .post(verifyToken, checkRole(["admin"]), adminController.createNewAdmin);
+router.route("/profile/:id").get(verifyToken, adminController.getProfile);
+router
+  .route("/")
+  .get(verifyToken, checkRole(["admin"]), adminController.getAllAdmins);
+router.route("/:id").get(verifyToken, adminController.getAdminById);
+router.route("/:id").put(verifyToken, adminController.updateAdmin);
+router
+  .route("/:id")
+  .delete(verifyToken, checkRole(["admin"]), adminController.deleteAdmin);
+router
+  .route("/:id/change-password")
+  .put(verifyToken, checkRole(["admin"]), adminController.changePassword);
 
 export default router;
